d7f332e188bcae38679ab312c59eec0cf6dfe73f
itaegyeong <tph00100@gmail.com>
2021-05-29 16:50:00 +0900

    [210529] class 4 essential 문제 남음

--- Baekjoon/Graph/2206.cpp
+++ Baekjoon/Graph/2206.cpp
@@ -2,68 +2,81 @@
 #include <algorithm>
 #include <vector>
 #include <queue>
+#include <string>
 using namespace std;
-struct location{
+vector<vector<vector<int> > > v(1001,vector<vector<int> >(1001,vector<int>(2,-1)));
+vector<vector<int> > map(1001,vector<int>(1001));
+struct data{
     int x,y;
+    bool v;
 };
-int map[1001][1001];
-vector<vector<vector<int> > > v(1001,vector<vector<int> >(1001,vector<int>(2,-1))); 
-int n,m;
 int x[5]={0,-1,0,1,0},y[5]={0,0,1,0,-1};
-queue<location> p;
+int n,m;
+queue<data> q;
 int bfs()
 {
-    p.push({1,1});
-    if(map[1][1]!=1)
-        v[1][1][0]=0;
-    v[1][1][1]=0;
-    while(!p.empty())
+    int i,j,k;
+    v[1][1][0]=1;
+    q.push({1,1,false});
+    while(!q.empty())
     {
-        for(int i=1; i<=4; i++)
+        data temp=q.front();
+        for(i=1; i<=4; i++)
         {
-            location temp=p.front();
             int tx=temp.x+x[i];
             int ty=temp.y+y[i];
             if(tx<=0 || ty<=0 || tx>n || ty>m)
                 continue;
-            if(map[tx][ty]==0 && v[temp.x][temp.y][0]!=-1 && (v[tx][ty][0]>v[temp.x][temp.y][0]+1 || v[tx][ty][0]==-1))
-            {
-                p.push({tx,ty});
-                v[tx][ty][0]=v[temp.x][temp.y][0]+1;
+            if(temp.v==false){ // 벽을 부수지 않은 상태
+                if(map[tx][ty]==1){ // 벽을 부술려고하는 경우
+                    if(v[tx][ty][1]==-1 || (v[tx][ty][1]>v[temp.x][temp.y][0]+1)){
+                        v[tx][ty][1]=v[temp.x][temp.y][0]+1;
+                        q.push({tx,ty,true});
+                    }
+                }
+                else{  // 벽을 부수지 않을려는 경우
+                    if(v[tx][ty][0]==-1 || (v[tx][ty][0]>v[temp.x][temp.y][0]+1)){
+                        v[tx][ty][0]=v[temp.x][temp.y][0]+1;
+                        q.push({tx,ty,false});
+                    }
+                }
             }
-            if(map[tx][ty]==0 && v[temp.x][temp.y][1]!=-1 && (v[tx][ty][1]>v[temp.x][temp.y][1]+1 || v[tx][ty][1]==-1))
-            {
-                p.push({tx,ty});
-                v[tx][ty][1]=v[temp.x][temp.y][1]+1;
-            }
-            if(map[tx][ty]==1 && v[temp.x][temp.y][0]!=-1 && (v[tx][ty][1]>v[temp.x][temp.y][0]+1 || v[tx][ty][1]==-1))
-            {
-                p.push({tx,ty});
-                v[tx][ty][1]=v[temp.x][temp.y][0]+1;
+            else{ //벽을 이미 부순 상태
+                if(map[tx][ty]==0){
+                    if(v[tx][ty][1]==-1 || (v[tx][ty][1]>v[temp.x][temp.y][1]+1)){
+                        v[tx][ty][1]=v[temp.x][temp.y][1]+1;
+                        q.push({tx,ty,true});
+                    }
+                }
             }
         }
-        p.pop();
+        q.pop();
     }
-    int result=max(v[n][m][0],v[n][m][1]);
-    if(result!=-1)
-        result++;
+    if(v[n][m][0]==-1 && v[n][m][1]==-1)
+        return -1;
+    if(v[n][m][0]!=-1 && v[n][m][1]==-1)
+        return v[n][m][0];
+    if(v[n][m][0]==-1 && v[n][m][1]!=-1)
+        return v[n][m][1];
+    int result=min(v[n][m][0],v[n][m][1]);
     return result;
 }
 int main()
 {
-    int i,j;
-    scanf("%d %d ",&n,&m);
-
+    int i,j,k;
+    scanf("%d %d",&n,&m);
+    
     for(i=1; i<=n; i++)
-    {  
+    {
         char temp[m+1];
+        
         scanf("%s",temp);
         for(j=1; j<=m; j++)
         {
             if(temp[j-1]=='0'){
                 map[i][j]=0;
             }
-            else{
+            else if(temp[j-1]=='1'){
                 map[i][j]=1;
             }
         }
