a4f6ca093492c52ad4ba637ce09e342cb1d46171
이태경 <tph00100@gmail.com>
2022-04-18 22:51:28 +0900

    [baekjoon] 14652 - bronze 5(스트릭용)

---
+++ Baekjoon/base/14652.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  ios_base::sync_with_stdio();
+  cin.tie(0);
+
+  int a, b, c;
+  cin >> a >> b >> c;
+
+  cout << c / b << " " << c % b;
+  return 0;
+}
