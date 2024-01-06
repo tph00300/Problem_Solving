971d2326220e6772cbc1dcf2142b2fb086793d92
TaeKyung Lee <tph00100@gmail.com>
2024-01-06 17:22:56 +0900

    BOJ 25083

---
+++ Baekjoon/구현/25083.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  cout << "         ,r\'\"7" << endl <<
+"r`-_   ,\'  ,/" << endl <<
+" \\. \". L_r\'" << endl <<
+"   `~\\/" << endl <<
+"      |" << endl <<
+"      |";
+  return 0;
+}
